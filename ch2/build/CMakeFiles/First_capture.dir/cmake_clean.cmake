b4ad50e6c12fb4cd424326b26cd373274efaca14
Jianbo-Guo <81056227+Jianbo-Guo@users.noreply.github.com>
2021-03-23 23:32:45 +0800

    Add files via upload

---
+++ ch2/build/CMakeFiles/First_capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/First_capture.dir/First_capture.cpp.o"
+  "First_capture.pdb"
+  "First_capture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/First_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch2/build/CMakeFiles/Second_camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Second_camera.dir/Second_camera.cpp.o"
+  "Second_camera.pdb"
+  "Second_camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Second_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
